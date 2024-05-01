d285152f2a1dbd4306c2c8a55240d252a553e936
cbookshu <cbookshu@outlook.com>
2024-05-01 18:38:46 +0800

    test close

--- example/sio.cpp
+++ example/sio.cpp
@@ -2,11 +2,7 @@
 //
 
 #include <iostream>
-#include "shuio/shu_loop.h"
-#include "shuio/shu_acceptor.h"
-#include "shuio/shu_stream.h"
-#include "shuio/shu_buffer.h"
-#include "shuio/shu_client.h"
+#include "shuio/shuio.h"
 #include <queue>
 #include <chrono>
 #include <functional>
@@ -16,73 +12,6 @@
 
 using namespace shu;
 using namespace std;
-
-struct hash_addr_storage_t {
-    std::size_t operator ()(const addr_storage_t& addr) const {
-        std::string_view sip(addr.ip.data());
-        return std::hash<const char*>()(sip.data()) ^ std::hash<int>()(addr.port);
-    }
-};
-
-struct equal_addr_storage_t {
-    bool operator()(const addr_storage_t& a, const addr_storage_t& b) const {
-        return std::tie(a.ip, a.port) == std::tie(b.ip, b.port);
-    }
-};
-
-// class tcp_client {
-// public:
-//     tcp_client(sloop& loop, addr_storage_t addr):loop_(loop) {
-//         sclient client;
-//         client.start(&loop_, addr, [this](socket_io_result_t res, std::unique_ptr<ssocket> sock, addr_pair_t addr) mutable {
-//             if (res.err) {
-//                 std::cout << "client connect error:" << res.naviteerr << std::endl;
-//                 return;
-//             }
-
-//             sstream stream;
-//             stream.start(&loop_, sock.release(), { .addr = addr }, [this](socket_io_result_t res, read_ctx_t& r) {
-//                 on_read(res, r);
-//             },
-//                 [this](socket_io_result_t res, write_ctx_t& w) {
-//                 on_write(res, w);
-//             });
-//             stream_.emplace(std::move(stream));
-//             on_connect();
-//         });
-//     }
-
-//     void write(socket_buffer buff) {
-//         if (stream_) {
-//             buff.commit();
-//             stream_.value().write(std::move(buff));
-//         }
-//     }
-
-//     void on_connect() {
-//         socket_buffer buff("hello world");
-//         write(std::move(buff));
-//     }
-
-//     void on_read(socket_io_result_t res, read_ctx_t& r) {
-//         if (res.err) {
-//             std::cout << "read err:" << res.naviteerr << endl;
-//             stream_.reset();
-//             return;
-//         }
-//         auto rd = r.buf.ready();
-//         std::string_view str(rd.data(), rd.size());
-//         std::cout << "client read:" << str << std::endl;
-//         r.buf.commit(rd.size());
-//     }
-
-//     void on_write(socket_io_result_t res, write_ctx_t& w) {
-
-//     }
-// private:
-//     sloop& loop_;
-//     std::optional<sstream> stream_;
-// };
 
 void start_timer() {
     sloop l({});
@@ -107,9 +36,88 @@ void start_timer() {
     std::cout << std::chrono::duration_cast<std::chrono::milliseconds>(n2-n1).count() << std::endl;
 }
 
+
+static char gMsg[] = "hello world";
+class tcp_server {
+public:
+    tcp_server(sloop& loop, addr_storage_t addr) 
+    :loop_(loop)
+    {
+        auto ok = server_.start(&loop_, {
+            .evClose = [](sacceptor*){},
+            .evConn = [this](socket_io_result_t res,
+                ssocket* sock,
+                addr_pair_t addr) {
+            on_client(res, sock, addr);}
+        }, addr);
+        shu::panic(ok);
+    }
+
+    ~tcp_server() {
+        server_.stop();
+    }
+
+    void on_client(socket_io_result_t res,
+        ssocket* sock,
+        addr_pair_t addr) {
+
+        std::unique_ptr<ssocket> ptr(sock);
+        if (res.res <= 0) {
+            std::cout << "tcp server err:" << strerror(-res.res) << std::endl;
+            server_.stop();
+            return;
+        }
+
+        auto stream_ptr = std::make_unique<sstream>();
+
+        stream_ptr->start(&loop_, ptr.release(), {.addr = addr}, {
+            .evClose = [](sstream* s){
+                delete s;
+            }
+        });
+        auto p = stream_ptr.release();
+        p->read([this, p](socket_io_result res, buffers_t bufs){
+            on_read(p, res, bufs);
+        },
+        [this, p](int size, buffer_t& buf){
+            // 可以是空的，不使用外部的buf
+        });
+
+        buffer_t buf;
+        buf.p = gMsg;
+        buf.size = strlen(gMsg);
+        p->write(buf, [this, p](socket_io_result res){
+            on_write(p, res);
+        });
+    }
+    
+    void on_read(sstream* s, socket_io_result_t res, buffers_t buf) {
+        if (res.res <= 0) {
+            // 3: 被对面关闭
+            s->stop();
+            return;
+        }
+
+        // 1: 读回调 stop
+        // s->stop();
+    }
+
+    void on_write(sstream* s, socket_io_result_t res) {
+
+        // 2: 写回调 stop
+        // s->stop();
+    }
+private:
+    sloop& loop_;
+    sacceptor server_;
+    std::pmr::unsynchronized_pool_resource pool_;
+};
+
 int main(int argc, char**argv)
 {
-    start_timer();
+    sloop loop;
+    tcp_server server(loop, {8888});
+    loop.run();
     return 0;
 }
 
