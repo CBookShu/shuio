8a4d13830c55664feb09e2b4b5900074b3d95124
cbookshu <cbookshu@outlook.com>
2024-05-04 14:00:45 +0800

    linux 下忽略SIGPIPE

--- shuio/linux/linux_detail.cpp
+++ shuio/linux/linux_detail.cpp
@@ -3,6 +3,15 @@
 #include "shuio/shu_loop.h"
 
 namespace shu {
+    struct global_init {
+        global_init() {
+            // 默认就给他处理掉吧，否则业务层还要判断平台再做操作。
+            ::signal(SIGPIPE, SIG_IGN);
+        }
+        ~global_init() {
+            
+        }
+    }G_wsa_initor;
 
     auto navite_cast_ssocket(ssocket* s) -> fd_navite_t* {
         return reinterpret_cast<fd_navite_t*>(s->handle());
